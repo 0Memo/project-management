a246da5e0744e498bf40124851a057a66977d54c
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-01 13:18:55 +0200

    updated server/src/index.ts & added server/ecosystem.config.js

---
+++ server/ecosystem.config.js
@@ -0,0 +1,10 @@
+export const apps = [
+    {
+        name: "project-management",
+        script: "npm",
+        args: "npm run dev",
+        env: {
+            NODE_ENV: "development",
+        }
+    }
+];
