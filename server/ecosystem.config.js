efd4eecaf92ca9b24206a1ecfb0bc5f05024a504
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-04-01 16:10:40 +0200

    updated ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ export const apps = [
     {
         name: "project-management",
         script: "npm",
-        args: "npm run dev",
+        args: "run dev",
         env: {
             NODE_ENV: "development",
         }
